224d9c7c96179615dc0bfec79098b2d8b7168eb9
isaev-bolsen <al_mamistov@mail.ru>
2017-02-10 23:14:27 +0300

    Testsprototype

---
+++ SorterTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SorterTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SorterTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("3f5d822c-f45d-4d72-8f3a-a008af15aeb3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SorterTest/SorterTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PX.Api.ContractBased.Maintenance.Cli.Utils;
+
+namespace SorterTest
+{
+    [TestClass]
+    public class SorterTest
+    {
+        [TestMethod]
+        public void TetSort1()
+        {
+            XDocument Doc = XDocument.Load("Sample1.xml");
+            Doc.Sort();
+
+        }
+    }
+}
